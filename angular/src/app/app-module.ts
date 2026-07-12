c4afca758edea1261e5bed4abefa27c76201c5d0
Frenchfryegr <frenchfrycaleb@gmail.com>
2026-07-12 16:49:58 -0500

    Add PWA Things

--- angular/src/app/app-module.ts
+++ angular/src/app/app-module.ts
@@ -1,7 +1,8 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { isDevMode, NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -10,7 +11,14 @@ import { Games } from './games/games/games';
 @NgModule({
   declarations: [App, Games],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  providers: [provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay())],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   bootstrap: [App],
 })
 export class AppModule {}
