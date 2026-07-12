673a34949afa7f4deaaada773fe87ae049cd6270
Frenchfryegr <frenchfrycaleb@gmail.com>
2026-07-11 19:51:11 -0500

    Add Basic Game Endpoints

--- api/main.py
+++ api/main.py
@@ -1,8 +1,19 @@
 import os
 from dotenv import load_dotenv
 
+from enum import Enum
+from datetime import datetime
+
 from fastapi import FastAPI
 from supabase import create_client, Client
+
+class TABLE_NAMES(Enum):
+    GAMES = "Games"
+    PLAYERS = "Players"
+    PLAYERSXGAMES = "Players_X_Games"
+    PLAYERROUNDSCORE = "PlayerRoundScore"
+    ROUNDS = "Rounds"
+
 
 load_dotenv()
 
@@ -17,3 +28,30 @@ supabase: Client = create_client(
 def read_root():
     return {"Bruh": "You made a request to the base endpoint. You prolly don't know what you're doing huh?"}
 
+@app.get("/games")
+def get_games():
+    response = (
+        supabase.table(TABLE_NAMES.GAMES.value)
+        .select()
+        .execute()
+    )
+    return response
+
+@app.post("/games/{game_id}")
+def insert_game(game_id):
+    response = (
+        supabase.table(TABLE_NAMES.GAMES.value)
+        .insert({"game_id": game_id, "game_datetime": datetime.now().isoformat()})
+        .execute()
+    )
+    return response
+
+@app.delete("/games/{game_id}")
+def delete_game(game_id):
+    response = (
+        supabase.table(TABLE_NAMES.GAMES.value)
+        .delete()
+        .eq("game_id", game_id)
+        .execute()
+    )
+    return response
